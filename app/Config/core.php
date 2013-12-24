76c0f26e7609038531b9e6ce2c972abeff176a4b
Giovanna Russo <russopgiovanna@gmail.com>
2013-12-24 16:04:36 -0430

    CakePHP framework added

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'wasiwasiwasilukipopopopopomierda');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '73651313213213213213213213165555665520');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
